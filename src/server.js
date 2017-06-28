50569c556cf2b4a3b48a7a574078c54dc9fd2e99
Tianhe Wu <twu@voltdb.com>
2017-06-28 15:33:01 -0400

    server working !

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
-// Load the http module to create an http server.
-var http = require('http');
+// var http = require('http');
+const express = require('express');
+const app = express();
+const path = require('path');
+
 var VoltClient = require('./voltjs/client');
 var VoltConfiguration = require('./voltjs/configuration');
 var VoltConstants = require('./voltjs/voltconstants');
@@ -81,14 +84,50 @@ AsyncPolling(function (end) {
 
 voltinit();
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+
+console.log(path.resolve('dist', './browser.js'));
+
+app.use((req, res, next) => {
+  console.log('Middleware 1...');
+  next();
 });
 
-// Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(3000);
+// The index page
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('dist', './index.html'));
+});
+
+// The main js script
+app.get('/browser.js', (req, res) => {
+  res.sendFile(path.resolve('dist', './browser.js'));
+});
+
+// In fonts folder
+app.get('/fonts/*.css', (req, res) => {
+  res.sendFile(path.resolve('dist', 'fonts', 'source-sans-pro.css')); // if you think hardcoding is bad, change it yourself
+});
+
+// All the other files should go to the current folder
+app.get('/*', (req, res) => {
+  console.log(path.resolve('dist', '.' + req.originalUrl));
+  res.sendFile(path.resolve('dist', '.' + req.originalUrl));
+});
+
+app.listen(3000, function () {
+  console.log('Hell yea...');
+});
+
+
+
+
+// // Configure our HTTP server to respond with Hello World to all requests.
+// var server = http.createServer(function (request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.end("Hello World\n");
+// });
+//
+// // Listen on port 8000, IP defaults to 127.0.0.1
+// server.listen(3000);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:3000/");
